b6611eb0443d9d1fc5e33d9c15ced4459c66c14c
tracer-nju <tracer@nju.edu.cn>
2023-01-30 02:12:16 -0500

    Linux normal 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:12:16 up 2 days, 16:58,  1 user,  load average: 0.68, 0.35, 0.16

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 //把系统中的进程按照父亲-孩子的树状结构打印到终端。 \
--p, --show-pids: 打印每个进程的进程号。 \
--n --numeric-sort: 按照pid的数值从小到大顺序输出一个进程的直接孩子。 \
--V --version: 打印版本信息。 \
-你可以在命令行中观察系统的 pstree 的执行行为 (如执行 pstree -V、pstree --show-pids 等)。这些参数可能任意组合，但你不需要处理单字母参数合并的情况，例如 -np。
+//-p, --show-pids: 打印每个进程的进程号。 \
+//-n --numeric-sort: 按照pid的数值从小到大顺序输出一个进程的直接孩子。 \
+//-V --version: 打印版本信息。 \
+//你可以在命令行中观察系统的 pstree 的执行行为 (如执行 pstree -V、pstree --show-pids 等)。这些参数可能任意组合，但你不需要处理单字母参数合并的情况，例如 -np。
 
 
 int main(int argc, char *argv[]) {
