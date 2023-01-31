a023416611cf12f2cd8a87a4bf7d35d83cd58ba1
tracer-nju <tracer@nju.edu.cn>
2023-01-31 03:00:44 -0500

    Linux normal 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:00:44 up 3 days, 17:40,  1 user,  load average: 0.72, 0.56, 0.47

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,9 +2,23 @@
 #include <assert.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#define _POSIX_SOURCE
+#include <dirent.h>
+#include <errno.h>
+#include <sys/types.h>
+#undef _POSIX_SOURCE
 
-void Usage(const char *filename);
-void Pstree_Version();
+
+#define BUF_SIZE 1024
+
+// declare function
+void usage(const char *filename);
+void pstree_version();
+void list_process();
+void getNameByPid(pid_t pid, char *task_name);
+
+
 int main(int argc, char *argv[]) {
   // for (int i = 0; i < argc; i++) {
   //   assert(argv[i]);
@@ -24,19 +38,18 @@ int main(int argc, char *argv[]) {
     case 'n':
       break;
     case 'V':
-      Pstree_Version();
+      pstree_version();
       break;
 
-    default:
-      
-      Usage(argv[0]);
+    default:      
+      usage(argv[0]);
       exit(EXIT_FAILURE);
     }
   }
-
+  list_process();
   if(optind >= argc){
     fprintf(stderr,"Expected argument after options\n");
-    Usage(argv[0]);
+    usage(argv[0]);
     exit(EXIT_FAILURE);
   }
 
@@ -45,7 +58,7 @@ int main(int argc, char *argv[]) {
 }
 
 
-void Usage(const char *filename){
+void usage(const char *filename){
   fprintf(stderr,"Usage: %s [-p, -n, -V]\n",filename);
   printf("把系统中的进程按照父亲-孩子的树状结构打印到终端。\n");
   printf("-p \t--show-pids: 打印每个进程的进程号。\n");
@@ -54,6 +67,50 @@ void Usage(const char *filename){
 }
 
 
-void Pstree_Version(){
+void pstree_version(){
   printf("pstree (simple) 1.0 Copyright (C) 2023-2023 \nThis is free software, and you are welcome to redistribute it under the terms of the GNU General Public License. \nFor more information about these matters, see the files named COPYING.\n");
 }
+
+void list_process(){
+DIR *pd;
+int fd;
+struct dirent *pdir;
+
+pd = opendir("/proc");
+while ((pdir = readdir(pd)) != NULL)
+    {
+        /* code */
+        char proc_name[50];
+        int pid = atoi(pdir->d_name);
+        if(pid != 0){
+       getNameByPid(pid,proc_name);
+        printf("%s\t %s\n",pdir->d_name,proc_name);   
+        }
+  }
+closedir(pd);
+// pid_t tp = getpid();
+// printf("%d\n",tp);
+// char tpname[50];
+// getNameByPid(tp,tpname);
+// printf("%s\n",tpname);
+// strcpy(tpname, argv[0]+2);
+// printf("task name is %s\n", tpname); 
+// return 0;
+}
+
+void getNameByPid(pid_t pid, char *task_name) {
+    char proc_pid_path[BUF_SIZE];
+    char buf[BUF_SIZE];
+
+    sprintf(proc_pid_path, "/proc/%d/status", pid);
+    //printf("%s\n",proc_pid_path);
+    FILE* fp = fopen(proc_pid_path, "r");
+    if(NULL != fp){
+        if( fgets(buf, BUF_SIZE-1, fp)== NULL ){
+            printf("fp == null\n");
+            fclose(fp);
+        }
+        fclose(fp);
+        sscanf(buf, "%*s %s", task_name);
+    }
+ }
