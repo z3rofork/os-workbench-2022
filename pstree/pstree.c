be0e18db182f52123e19c9315122d95645451ddb
tracer-nju <tracer@nju.edu.cn>
2023-01-30 21:03:08 -0500

    Linux normal 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:03:08 up 3 days, 11:42,  1 user,  load average: 0.73, 0.81, 0.65

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,18 +1,52 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-/*把系统中的进程按照父亲-孩子的树状结构打印到终端。 \
-//-p, --show-pids: 打印每个进程的进程号。 \
-//-n --numeric-sort: 按照pid的数值从小到大顺序输出一个进程的直接孩子。 \
-//-V --version: 打印版本信息。 \
-//你可以在命令行中观察系统的 pstree 的执行行为 (如执行 pstree -V、pstree --show-pids 等)。这些参数可能任意组合，但你不需要处理单字母参数合并的情况，例如 -np。
-*/
-
+void Usage(const char *filename);
+void Pstree_Version();
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+  // for (int i = 0; i < argc; i++) {
+  //   assert(argv[i]);
+  //   printf("argv[%d] = %s\n", i, argv[i]);
+  // }
+  // assert(!argv[argc]);
+  int opt;
+  const char *optString="p::n::V::";
+  while ((opt = getopt(argc,argv,optString)) != -1)
+  {
+    /* code */
+    switch (opt)
+    {
+    case 'p':
+      /* code */
+      break;
+    case 'n':
+      break;
+    case 'V':
+      Pstree_Version();
+      break;
+
+    default:
+      
+      Usage(argv[0]);
+      exit(EXIT_FAILURE);
+    }
   }
-  assert(!argv[argc]);
+  exit(EXIT_SUCCESS);
   return 0;
+}
+
+
+void Usage(const char *filename){
+  fprintf(stderr,"Usage: %s [-p, -n, -V]\n",filename);
+  printf("把系统中的进程按照父亲-孩子的树状结构打印到终端。");
+  printf("-p \t--show-pids: 打印每个进程的进程号。");
+  printf("-n \t--numeric-sort: 按照pid的数值从小到大顺序输出一个进程的直接孩子。\n");
+  printf("-V \t--version: 打印版本信息。"); 
+}
+
+
+void Pstree_Version(){
+  printf("pstree (simple) 1.0 Copyright (C) 2023-2023 \nThis is free software, and you are welcome to redistribute it under the terms of the GNU General Public License. \nFor more information about these matters, see the files named COPYING.");
 }
