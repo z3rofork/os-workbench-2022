2874ea24e072bbfd67af2b186e3bd0cdcaffb53e
z3rofork <z3rofork@protonmail.com>
2023-02-23 03:00:16 -0500

    begin M3

--- Makefile
+++ Makefile
@@ -10,10 +10,8 @@ SRCS   := $(shell find . -maxdepth 1 -name "*.c")
 DEPS   := $(shell find . -maxdepth 1 -name "*.h") $(SRCS)
 CFLAGS += -O1 -std=gnu11 -ggdb -Wall -Werror -Wno-unused-result -Wno-unused-value -Wno-unused-variable
 
-.PHONY: all git test clean commit-and-make
+.PHONY: clean 
 
-.DEFAULT_GOAL := commit-and-make
-commit-and-make: git all
 
 $(NAME)-64: $(DEPS) # 64bit binary
 	gcc -m64 $(CFLAGS) $(SRCS) -o $@ $(LDFLAGS)
@@ -30,4 +28,3 @@ $(NAME)-32.so: $(DEPS) # 32bit shared library
 clean:
 	rm -f $(NAME)-64 $(NAME)-32 $(NAME)-64.so $(NAME)-32.so
 
-include ../Makefile.lab
--- sperf/sperf.c
+++ sperf/sperf.c
@@ -2,12 +2,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+extern char **environ;
+
 int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace", "ls", NULL, };
-  char *exec_envp[] = { "PATH=/bin", NULL, };
-  execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
+  //parse args
+  char *exec_argv[] = {};
+  char **exec_envp = environ;
+  int env_num = 0;
+
+  exec_argv[0] = "/usr/bin/strace";
+  exec_argv[1] = "-c -U time,syscall";
+  for (int i = 2; i < argc; i++)  { exec_argv[i] = argv[i]; }
+  exec_argv[argc+1] = NULL;
+  for(env_num;exec_envp[env_num] != 0;env_num++)  {}
+  exec_envp[env_num+1] = NULL;
+
+  if(fork() == 0){
+    execve("/bin/strace",exec_argv,exec_envp);
+  }
+
+  //parse args
+
+
+  //execve("strace",          exec_argv, exec_envp);
+  //execve("/bin/strace",     exec_argv, exec_envp);
+  //execve("/usr/bin/strace", exec_argv, exec_envp);
+  printf("%s\n",exec_argv[0]);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
