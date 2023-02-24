4d14a3fbad6f058570e6084a9bf5eecc64f00336
z3rofork <z3rofork@protonmail.com>
2023-02-24 02:36:35 -0500

    2nd part finished

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <fcntl.h>
+
+#define SIZE 4096
 extern char **environ;
 
+
 int main(int argc, char *argv[]) {
-    if(argc != 2){
-    printf("Usage: %s <String>\n",argv[0]);
-    exit(EXIT_FAILURE);
-  }
   //parse args
   int exec_argc = 4;
   pid_t pid;
@@ -19,17 +19,46 @@ int main(int argc, char *argv[]) {
     NULL,
     };
   int env_num = 0;
+  int pipefd[2];
+
+  if(argc != 2){
+    printf("Usage: %s <String>\n",argv[0]);
+    exit(EXIT_FAILURE);
+  }
   //receive and parse args
   for (;exec_argc - 3 < argc; exec_argc++)  { exec_argv[exec_argc] = argv[exec_argc-3];}
   exec_argv[exec_argc++] = NULL;
-  
-  if((pid = fork()) == 0){
+
+  //error handlering  
+  if(pipe(pipefd) != 0){
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+
+  //begin create new process
+  pid = fork();
+  if(pid == 0){
+    //pipe control
+    close(1);
+    dup(pipefd[1]);
     execve("/bin/strace",exec_argv,exec_envp);
     //printf("%d\n",pid);
+    close(0);
+    close(pipefd[1]);
   }
-  else{
-    //printf("%d\n",getpid());
+  else if (pid ==-1)  { perror("fork");exit(EXIT_FAILURE);}
+
+  else {
+    close(0);
+    dup(pipefd[0]);
+    printf("this is me !!!!!\n");
+    wait(NULL);
+    printf("this is me too !!!!!\n");
+
+    close(pipefd[0]);
+    close(pipefd[1]);
   }
+
 
 
 
