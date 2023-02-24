d7a0d879be4e8fd0016988f8d1831f61e05ab49f
z3rofork <z3rofork@protonmail.com>
2023-02-23 21:35:32 -0500

    parse args finished

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,33 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <sys/types.h>
+#include <fcntl.h>
 extern char **environ;
 
 int main(int argc, char *argv[]) {
+    if(argc != 2){
+    printf("Usage: %s <String>\n",argv[0]);
+    exit(EXIT_FAILURE);
+  }
   //parse args
-  char *exec_argv[] = {};
-  char **exec_envp = environ;
+  int exec_argc = 4;
+  pid_t pid;
+  char *exec_argv[] = {"strace","-c","-U","time,syscall",};
+  char *exec_envp[] = {
+    "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin",
+    NULL,
+    };
   int env_num = 0;
-
-  exec_argv[0] = "/usr/bin/strace";
-  exec_argv[1] = "-c -U time,syscall";
-  for (int i = 2; i < argc; i++)  { exec_argv[i] = argv[i]; }
-  exec_argv[argc+1] = NULL;
-  for(env_num;exec_envp[env_num] != 0;env_num++)  {}
-  exec_envp[env_num+1] = NULL;
-
-  if(fork() == 0){
+  //receive and parse args
+  for (;exec_argc - 3 < argc; exec_argc++)  { exec_argv[exec_argc] = argv[exec_argc-3];}
+  exec_argv[exec_argc++] = NULL;
+  
+  if((pid = fork()) == 0){
     execve("/bin/strace",exec_argv,exec_envp);
+    //printf("%d\n",pid);
+  }
+  else{
+    //printf("%d\n",getpid());
   }
 
-  //parse args
 
 
   //execve("strace",          exec_argv, exec_envp);
   //execve("/bin/strace",     exec_argv, exec_envp);
   //execve("/usr/bin/strace", exec_argv, exec_envp);
-  printf("%s\n",exec_argv[0]);
-  perror(argv[0]);
+  //printf("%s\n",exec_argv[0]);
+  //perror(argv[0]);
   exit(EXIT_FAILURE);
 }
